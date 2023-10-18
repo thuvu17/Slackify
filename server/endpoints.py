76fc26a35b5e6f0577c35fa51ac6883fa5659f03
Thu Vu <tnv2002@nyu.edu>
2023-10-18 16:16:12 -0400

    Update endpoints.py

--- server/endpoints.py
+++ server/endpoints.py
@@ -5,17 +5,33 @@ The endpoint called `endpoints` will return all available endpoints.
 
 from flask import Flask
 from flask_restx import Resource, Api
-# import db.db as db
+
+import data.games as gms
+import data.users as users
 
 app = Flask(__name__)
 api = Api(app)
 
-MAIN_MENU = 'MainMenu'
+DEFAULT = 'Default'
+MENU = 'menu'
+MAIN_MENU_EP = '/MainMenu'
 MAIN_MENU_NM = "Welcome to Text Game!"
-USERS = 'users'
+HELLO_EP = '/hello'
+HELLO_RESP = 'hello'
+GAMES_EP = '/games'
+GAME_MENU_EP = '/game_menu'
+GAME_MENU_NM = 'Game Menu'
+# USERS = 'users'
+USERS_EP = '/users'
+USER_MENU_EP = '/user_menu'
+USER_MENU_NM = 'User Menu'
+TYPE = 'Type'
+DATA = 'Data'
+TITLE = 'Title'
+RETURN = 'Return'
 
 
-@api.route('/hello')
+@api.route(HELLO_EP)
 class HelloWorld(Resource):
     """
     The purpose of the HelloWorld class is to have a simple test to see if the
@@ -26,7 +42,7 @@ class HelloWorld(Resource):
         A trivial endpoint to see if the server is running.
         It just answers with "hello world."
         """
-        return {'hello': 'world'}
+        return {HELLO_RESP: 'world'}
 
 
 @api.route('/endpoints')
@@ -43,8 +59,7 @@ class Endpoints(Resource):
         return {"Available endpoints": endpoints}
 
 
-@api.route(f'/{MAIN_MENU}')
-@api.route('/')
+@api.route(f'{MAIN_MENU_EP}')
 class MainMenu(Resource):
     """
     This will deliver our main menu.
@@ -53,26 +68,77 @@ class MainMenu(Resource):
         """
         Gets the main game menu.
         """
-        return {'Title': MAIN_MENU_NM,
-                'Default': 2,
+        return {TITLE: MAIN_MENU_NM,
+                DEFAULT: 2,
                 'Choices': {
                     '1': {'url': '/', 'method': 'get',
                           'text': 'List Available Characters'},
                     '2': {'url': '/',
                           'method': 'get', 'text': 'List Active Games'},
-                    '3': {'url': f'/{USERS}',
+                    '3': {'url': f'{USERS_EP}',
                           'method': 'get', 'text': 'List Users'},
+                    '4': {'url': '/',
+                          'method': 'get', 'text': 'Illustrating a Point!'},
                     'X': {'text': 'Exit'},
                 }}
 
 
-@api.route(f'/{USERS}')
+@api.route(f'{USER_MENU_EP}')
+class UserMenu(Resource):
+    """
+    This will deliver our user menu.
+    """
+    def get(self):
+        """
+        Gets the user menu.
+        """
+        return {
+                   TITLE: USER_MENU_NM,
+                   DEFAULT: '0',
+                   'Choices': {
+                       '1': {
+                            'url': '/',
+                            'method': 'get',
+                            'text': 'Get User Details',
+                       },
+                       '0': {
+                            'text': 'Return',
+                       },
+                   },
+               }
+
+
+@api.route(f'{USERS_EP}')
 class Users(Resource):
     """
-    This class supports fetching a list of all pets.
+    This class supports fetching a list of all users.
     """
     def get(self):
         """
         This method returns all users.
         """
-        return 'Current Users:\nSai\nAbhishek\nKristian\n'
+        return {
+            TYPE: DATA,
+            TITLE: 'Current Games',
+            DATA: users.get_users(),
+            MENU: USER_MENU_EP,
+            RETURN: MAIN_MENU_EP,
+        }
+
+
+@api.route(f'{GAMES_EP}')
+class Games(Resource):
+    """
+    This class supports fetching a list of all games.
+    """
+    def get(self):
+        """
+        This method returns all games.
+        """
+        return {
+            TYPE: DATA,
+            TITLE: 'Current Games',
+            DATA: gms.get_games(),
+            MENU: GAME_MENU_EP,
+            RETURN: MAIN_MENU_EP,
+        }
