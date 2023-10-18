0e37e1021aea3e0f4892e7878d1260f3ae7c354a
JoyZhu219 <yz6985@nyu.edu>
2023-10-18 12:02:13 -0400

    add main.yaml

--- local.sh
+++ local.sh
@@ -6,4 +6,4 @@ export DEBUG=1
 
 # run our server locally:
 PYTHONPATH=$(pwd):$PYTHONPATH
-FLASK_APP=server.endpoints flask run --debug --host=127.0.0.1 --port=8000
+FLASK_APP=server.endpoints flask run --debugger --host=127.0.0.1 --port=8000
