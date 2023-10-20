6f68f82dd6dd3fe0986d6e9cf58863df3ca00827
Quan Phung <quan.phung@ecotruck.vn>
2023-10-20 14:36:13 -0400

    added add_name function to games.py and more test cases to test_games.py

--- data/games.py
+++ data/games.py
@@ -2,8 +2,26 @@
 games.py: the interface to our game data.
 """
 
-games = {}
+NUM_PLAYERS = 'numPlayers'
+TEST_GAME_NAME = 'Wizards Galore'
+
+games = {
+    'Dungeons and Dragons': {
+        NUM_PLAYERS: 3,
+    },
+    TEST_GAME_NAME: {
+        NUM_PLAYERS: 5,
+    },
+}
 
 
 def get_games() -> dict:
     return games
+
+
+def add_game(name: str, num_players: int):
+    if name in games:
+        raise ValueError(f'Duplicate game name: {name=}')
+    if not name:
+        raise ValueError('Game name may not be blank')
+    games[name] = {NUM_PLAYERS: num_players}
--- data/tests/test_games.py
+++ data/tests/test_games.py
@@ -1,6 +1,31 @@
-import data.games as gm
+import pytest
+
+import data.games as gms
 
 
 def test_get_games():
-    games = gm.get_games()
+    games = gms.get_games()
     assert isinstance(games, dict)
+    assert len(games) > 0
+    for game in games:
+        assert isinstance(game, str)
+        assert isinstance(games[game], dict)
+    assert gms.TEST_GAME_NAME in games
+
+
+def test_add_game_dup_name():
+    with pytest.raises(ValueError):
+        gms.add_game(gms.TEST_GAME_NAME, 4)
+
+
+def test_add_game_blank_name():
+    with pytest.raises(ValueError):
+        gms.add_game('', 4)
+
+
+ADD_NAME = 'New Game'
+
+
+def test_add_game():
+    gms.add_game(ADD_NAME, 4)
+    assert ADD_NAME in gms.get_games()
