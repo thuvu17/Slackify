e26cfc22ab3f38064c6b3172ebaa4e8bc299175a
Thu Vu <tnv2002@nyu.edu>
2023-10-29 16:16:27 -0400

    Added interface for songs data

---
+++ data/songs.py
@@ -0,0 +1,41 @@
+"""
+songs.py: the interface to our song data.
+"""
+
+TEST_SONG_NAME = 'Beat It'
+TEST_ARTIST_NAME = 'Michael Jackson'
+
+# Test songs
+songs = {
+    'ABC123': {
+        'name': 'Billie Jean',
+        'artist': 'Michael Jackson',
+        'album': 'idk',
+        'genre': 'Pop',
+        'bpm': 116,
+    },
+    'BCD456': {
+        'name': TEST_SONG_NAME,
+        'artist': TEST_ARTIST_NAME,
+        'album': 'idk2',
+        'genre': 'Pop',
+        'bpm': 125,
+    },
+}
+
+
+def get_songs() -> dict:
+    return songs
+
+def already_exist(song_data: dict):
+    for song in songs:
+        if song['name'] == song_data['name'] and song['artist'] == song_data['artist']:
+            return True
+    return False
+
+
+def add_song(song_id: str, song_data: dict):
+    # Check if a song with same name + artist is already in the database
+    if already_exist(song_data): 
+        raise ValueError("A song with the same name and artist already existed!")
+    songs[song_id] = song_data
