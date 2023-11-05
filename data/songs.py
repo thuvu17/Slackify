89b5efb5f7c6b6e8b11b129b4b2d74a451bdeeee
Quan Phung <quan.phung@ecotruck.vn>
2023-11-05 16:53:24 -0500

    update songs.py

--- data/songs.py
+++ data/songs.py
@@ -1,6 +1,18 @@
 """
 songs.py: the interface to our song data.
 """
+import random
+
+ID_LEN = 24
+BIG_NUM = 100000000000000
+MOCK_ID = '0'*ID_LEN
+NAME = 'name'
+SONG_NAME = 'song_name'
+ARTIST = 'artist'
+ALBUM = 'album'
+GENRE = 'genre'
+BPM = 'bpm'
+
 
 TEST_SONG_NAME = 'Beat It'
 TEST_ARTIST_NAME = 'Michael Jackson'
@@ -23,6 +35,29 @@ songs = {
     },
 }
 
+def _get_test_name():
+    name = 'test'
+    rand_part = random.randint(0, BIG_NUM)
+    return name + str(rand_part)
+
+
+def get_test_song():
+    test_song = {}
+    test_song[NAME] = _get_test_name()
+    test_song[SONG_NAME] = "Generic song name"
+    test_song[ARTIST] = "Popular artist"
+    test_song[ALBUM] = "idk"
+    test_song[GENRE] = "Pop"
+    test_song[BPM] = 116
+    return test_song
+
+
+def _gen_id() -> str:
+    _id = random.randint(0, BIG_NUM)
+    _id = str(_id)
+    _id = _id.rjust(ID_LEN, '0')
+    return _id
+
 
 def get_songs() -> dict:
     return songs
@@ -36,10 +71,11 @@ def already_exist(song_data: dict):
     return False
 
 
-def add_song(song_id: str, song_data: dict):
+def add_song(song_id: str, song_data: dict) -> str:
     # Check if a song with same name + artist
     # is already in the database
     if already_exist(song_data):
         raise ValueError("A song with the same name \
                          sand artist already existed!")
     songs[song_id] = song_data
+    return _gen_id()
