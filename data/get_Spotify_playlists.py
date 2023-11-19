31584ec553c356d130c84ef3602025357172844d
Quan Phung <quan.phung@ecotruck.vn>
2023-11-19 11:55:54 -0500

    test Spotify API by requesting a playlist from server

---
+++ data/get_Spotify_playlists.py
@@ -0,0 +1,16 @@
+import requests
+import get_Spotify_token
+
+token = get_Spotify_token.get_token()
+base_url = 'https://api.spotify.com/v1/'
+headers = {
+    'Authorization': 'Bearer {}'.format(access_token)
+}
+featured_playlists_endpoint = 'browse/featured-playlists/?limit=50'
+featured_playlists_url = ''.join([base_url,featured_playlists_endpoint])
+
+def get_featured_playlist():
+    response = requests.get(featured_playlists_url,headers=headers)
+    playlists = response.json().get('playlists').get('items')
+    return playlists
+
