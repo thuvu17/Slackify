5c8ed598d19801691857106fdc57bc035f26944d
Quan Phung <quan.phung@ecotruck.vn>
2023-11-19 12:04:41 -0500

    fixed bugs

--- data/get_Spotify_playlists.py
+++ data/get_Spotify_playlists.py
@@ -1,7 +1,7 @@
 import requests
 import get_Spotify_token
 
-token = get_Spotify_token.get_token()
+access_token = get_Spotify_token.get_token()
 base_url = 'https://api.spotify.com/v1/'
 
 headers = {
@@ -9,10 +9,10 @@ headers = {
 }
 
 featured_playlists_endpoint = 'browse/featured-playlists/?limit=50'
-featured_playlists_url = ''.join([base_url,featured_playlists_endpoint])
+featured_playlists_url = ''.join([base_url, featured_playlists_endpoint])
 
 
 def get_featured_playlist():
-    response = requests.get(featured_playlists_url,headers=headers)
+    response = requests.get(featured_playlists_url, headers=headers)
     playlists = response.json().get('playlists').get('items')
     return playlists
