aa19e5d5a46690d4ebd81e9c06f9648fdc3f7a50
Quan Phung <quan.phung@ecotruck.vn>
2023-11-26 18:27:32 -0500

    Added test for get_token file. Added more libraries in requirements.txt

--- data/tests/test_get_token.py
+++ data/tests/test_get_token.py
@@ -1,3 +1,11 @@
 import pytest
 
 import data.get_Spotify_token as tkn
+
+
+@pytest.fixture(scope='function')
+def get_token():
+    token = tkn.get_token()
+    assert isinstance(token, str)
+    assert len(token) > 0
+    
