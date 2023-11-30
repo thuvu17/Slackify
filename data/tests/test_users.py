5746a3bbacb50ba316bfb419161c0d44e5e66a39
Yongwen Lei <yl7690@nyu.edu>
2023-11-30 18:44:44 -0500

    added already_exist(), add_user(), and del_user() in users.py and their related tests in test_users.py

--- data/tests/test_users.py
+++ data/tests/test_users.py
@@ -1,4 +1,31 @@
+import pytest
+
 import data.users as usrs
+
+
+@pytest.fixture(scope='function')
+def temp_user():
+    user = usrs.get_test_user()
+    ret = usrs.add_user(user)
+    yield user
+    if usrs.already_exist(user['email']):
+        usrs.del_user(user['email'])
+
+
+def test_get_test_name():
+    name = usrs._get_test_name()
+    assert isinstance(name, str)
+    assert len(name) > 0
+
+
+def test_get_test_email():
+    name = usrs._get_test_email()
+    assert isinstance(name, str)
+    assert len(name) > 0
+
+
+def test_get_test_user():
+    assert isinstance(usrs.get_test_user(), dict)
 
 
 def test_get_users():
@@ -12,3 +39,55 @@ def test_get_users():
         assert isinstance(user, dict)
         assert usrs.EMAIL in user
         assert isinstance(user[usrs.EMAIL], str)
+
+
+def test_add_user():
+    new_name = usrs._get_test_name()
+    new_email = usrs._get_test_email()
+    new_user = {
+        'name': new_name,
+        'email': new_email,
+    }
+    ret = usrs.add_user(new_user)
+    assert usrs.already_exist(new_user['email'])
+    assert isinstance(ret, bool)
+    usrs.del_user(new_user['email'])
+
+
+def test_add_user_dup_email(temp_user):
+    with pytest.raises(ValueError):
+        usrs.add_user(temp_user)
+
+
+def test_add_user_lt_2_char():
+    new_name = 'w'
+    new_email = usrs._get_test_email()
+    new_user = {
+        'name': new_name,
+        'email': new_email,
+    }
+    with pytest.raises(ValueError):
+        usrs.add_user(new_user)
+
+
+def test_add_user_invalid_email():
+    new_name = usrs._get_test_name()
+    new_email = 'randomstring'
+    new_user = {
+        'name': new_name,
+        'email': new_email,
+    }
+    with pytest.raises(ValueError):
+        usrs.add_user(new_user)
+
+
+def test_del_user(temp_user):
+    email = temp_user['email']
+    usrs.del_user(email)
+    assert usrs.already_exist(email) is False
+
+
+def test_del_user_not_there():
+    email = usrs._get_test_email()
+    with pytest.raises(ValueError):
+        usrs.del_user(email)
--- data/users.py
+++ data/users.py
@@ -1,11 +1,37 @@
 """
 This module interfaces to our user data.
 """
+import random
+
 import data.db_connect as dbc
 
-EMAIL = "email"
+NAME = 'name'
+EMAIL = 'email'
+BIG_NUM = 100000000000000
 MIN_USER_NAME_LEN = 2
 USER_COLLECT = "users"
+
+
+# Return random user name
+def _get_test_name():
+    name = 'test'
+    rand_part = random.randint(0, BIG_NUM)
+    return name + str(rand_part)
+
+
+# Return random user name
+def _get_test_email():
+    name = 'test'
+    rand_part = random.randint(0, BIG_NUM)
+    email_suffix = '@gmail.com'
+    return name + str(rand_part) + email_suffix
+
+
+def get_test_user():
+    test_user = {}
+    test_user[NAME] = _get_test_name()
+    test_user[EMAIL] = _get_test_email()
+    return test_user
 
 
 def get_users():
@@ -31,3 +57,32 @@ def get_old_users():
         },
     }
     return users
+
+
+# Return fetched song as doc if found, else return false
+def already_exist(user_email: str):
+    dbc.connect_db()
+    fetched_song = dbc.fetch_one(USER_COLLECT, {EMAIL: user_email})
+    return fetched_song is not None
+
+
+def add_user(user_data: dict) -> bool:
+    # Check if a song with same name + artist
+    # is already in the database
+    if already_exist(user_data[EMAIL]):
+        raise ValueError("A user with the same email already existed!")
+    if len(user_data[NAME]) < 2:
+        raise ValueError("Minimum user name length is 2 characters!")
+    if '@' not in user_data[EMAIL]:
+        raise ValueError("Please enter a valid email!")
+    dbc.connect_db()
+    _id = dbc.insert_one(USER_COLLECT, user_data)
+    return _id is not None
+
+
+def del_user(user_email: str):
+    if already_exist(user_email):
+        return dbc.del_one(USER_COLLECT, {EMAIL: user_email})
+    else:
+        raise ValueError(f"Delete failure: User with email {user_email} "
+                         "not in database.")
