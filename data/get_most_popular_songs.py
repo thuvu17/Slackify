1021394f82b145520ffd4d2878aad4e3f924e742
Quan Phung <quan.phung@ecotruck.vn>
2023-12-10 22:37:58 -0500

    added get_most_popular_songs.py

---
+++ data/get_most_popular_songs.py
@@ -0,0 +1,20 @@
+import requests
+import data.get_Spotify_token as get_Spotify_token
+
+# Get top 10 most listened songs on Spotify
+access_token = get_Spotify_token.get_token()
+base_url = 'https://api.spotify.com/v1/'
+
+headers = {
+    'Authorization': 'Bearer {}'.format(access_token)
+}
+
+featured_playlists_endpoint = 'playlists/2YRe7HRKNRvXdJBp9nXFza/tracks?offset=0&limit=10'
+featured_playlists_url = ''.join([base_url, featured_playlists_endpoint])
+
+
+def get_most_popular_songs():
+    response = requests.get(featured_playlists_url, headers=headers)
+
+    songs = response.json().get('items')
+    return songs
