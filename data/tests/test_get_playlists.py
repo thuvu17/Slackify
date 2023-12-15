b68364a7cec48d74cb7af2f16d4d89179dc9cf75
Quan Phung <quan.phung@ecotruck.vn>
2023-12-15 14:09:39 -0500

    fixed error on Spotify endpoint tests

--- data/tests/test_get_playlists.py
+++ data/tests/test_get_playlists.py
@@ -1,9 +1,6 @@
-import pytest
-
 import data.get_Spotify_playlists as playlists
 
 
-@pytest.fixture(scope='function')
 def test_get_featured_playlist():
    featured_playlists = playlists.get_featured_playlists()
    assert isinstance(featured_playlists, list)
--- data/tests/test_get_token.py
+++ data/tests/test_get_token.py
@@ -1,9 +1,6 @@
-import pytest
-
 import data.get_Spotify_token as tkn
 
 
-@pytest.fixture(scope='function')
 def test_get_token():
    token = tkn.get_token()
    assert isinstance(token, str)
