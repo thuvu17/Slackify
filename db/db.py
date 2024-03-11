b8efd1ed8f44c6cf2a3e823fffa25432d3e611bc
Thu Vu <tnv2002@nyu.edu>
2024-03-11 15:50:46 -0400

    added functions to fetch song from spotify into db

--- db/db.py
+++ db/db.py
@@ -1,12 +1,25 @@
 """
 This file will manage interactions with our data store.
-At first, it will just contain stubs that return fake data.
-Gradually, we will fill in actual calls to our datastore.
 """
+import data.get_Spotify_specific_track as spotify_track
+import data.songs as songs
+
+new_track_ids = ['7qiZfU4dY1lWllzX7mPBI3', '1fidCEsYlaVE3pHwKCvpFZ',
+                 '6fNhZRFEkBfgW39W3wKARJ', '4gbVRS8gloEluzf0GzDOFc']
 
 
-def fetch_pets():
+def push_song_to_db(track_id):
     """
-    A function to return all pets in the data store.
+    A function to push data into db
     """
-    return {"tigers": 2, "lions": 3, "zebras": 1}
+    content = spotify_track.get_specific_track(track_id)
+    songs.add_song(content)
+
+
+def main():
+    for track_id in new_track_ids:
+        push_song_to_db(track_id)
+
+
+if __name__ == '__main__':
+    main()
