8ef7c996bdcaf8fe43c8347f5aa1c5c693cb7ccd
Quan Phung <quan.phung@ecotruck.vn>
2024-05-06 15:17:20 -0400

    test_get_playlists and test_get_popular_songs

--- data/tests/test_get_playlists.py
+++ data/tests/test_get_playlists.py
@@ -1,7 +1,7 @@
-# import data.get_Spotify_playlists as playlists
+import data.get_Spotify_playlists as playlists
 
 
-# def test_get_featured_playlist():
-#    featured_playlists = playlists.get_featured_playlists()
-#    assert isinstance(featured_playlists, list)
-#    assert len(featured_playlists) > 0
+def test_get_featured_playlist():
+   featured_playlists = playlists.get_featured_playlists()
+   assert isinstance(featured_playlists, list)
+   assert len(featured_playlists) > 0
