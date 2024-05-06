62123ca90979723cde7aac0417fed5ad9cdb2b3b
Quan Phung <quan.phung@ecotruck.vn>
2024-05-06 15:06:58 -0400

    uncomment get token test

--- data/tests/test_get_token.py
+++ data/tests/test_get_token.py
@@ -1,8 +1,8 @@
-# import data.get_Spotify_token as tkn
+import data.get_Spotify_token as tkn
 
 
-# def test_get_token():
-#    token = tkn.get_token()
-#    assert isinstance(token, str)
-#    assert len(token) > 0
+def test_get_token():
+   token = tkn.get_token()
+   assert isinstance(token, str)
+   assert len(token) > 0
     
